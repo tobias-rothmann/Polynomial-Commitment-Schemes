9804f01a9058ae83d42de56e85b93805129ca8dc
tobias_rothmann <ulrxz@student.kit.edu>
2023-04-23 20:00:54 +0200

    f_eq_xu_qx with q as Isabelle function, not poly

--- Commitment_Scheme.thy
+++ Commitment_Scheme.thy
@@ -51,13 +51,26 @@ ein Element k aus (Z_p)[x]^{<d} : "k :: 'a qr"
 *)
 
 (*  \<exists>q. \<phi>(x)-\<phi>(u) = (x-u)*q(x)  *)
-(*                                   (of_qr q)::('a mod_ring poly)   *)
-theorem f_eq_xu_qx: "\<forall>\<phi>::'a qr.  \<exists>q::'a qr. poly (of_qr \<phi>) x - poly (of_qr \<phi>) u = (x-u) * (poly (of_qr q) x)"
+theorem f_eq_xu_qx: "\<forall>\<phi>::'a qr.  \<exists>q. poly (of_qr \<phi>) x - poly (of_qr \<phi>) u = (x-u) * q x"
 proof
-  fix \<phi>
-  fix u x
-  show "\<exists>q. poly (of_qr \<phi>) x - poly (of_qr \<phi>) u = (x - u) * poly (of_qr q) x"
-      sorry
+  fix \<phi>::"'a qr"
+  fix u x :: "'a mod_ring"
+  obtain d where d: "d= deg_qr (TYPE('a))" by blast
+  obtain p_\<phi> where p_\<phi>: "(p_\<phi>::'a :: qr_spec mod_ring poly) = (of_qr \<phi>)" by blast
+  show "\<exists>q. poly (of_qr \<phi>) x - poly (of_qr \<phi>) u = (x - u) * q x"
+  proof 
+    let ?q ="(\<lambda>x. (\<Sum>i\<le>degree p_\<phi>. poly.coeff p_\<phi> i * (\<Sum>j<i. u^(i - Suc j) * x^j)))"
+    (*idee: über endl. Summen, see: poly_as_sum *)
+    have "(\<Sum>i\<le>degree p_\<phi>. poly.coeff p_\<phi> i * x ^ i) - (\<Sum>i\<le>degree p_\<phi>. poly.coeff p_\<phi> i * u ^ i) 
+      = (\<Sum>i\<le>degree p_\<phi>. poly.coeff p_\<phi> i * (x ^ i - u ^ i))"
+      by (simp add: sum_subtractf right_diff_distrib')
+    also have "\<dots> = (\<Sum>i\<le>degree p_\<phi>. (x - u) * poly.coeff p_\<phi> i * (\<Sum>j<i. u^(i - Suc j) * x^j))"
+      by (simp add: mult.assoc mult.left_commute power_diff_sumr2)
+    also have "\<dots> = (x - u) * (\<Sum>i\<le>degree p_\<phi>. poly.coeff p_\<phi> i * (\<Sum>j<i. u^(i - Suc j) * x^j))" 
+      by (metis (no_types, lifting) mult_hom.hom_sum mult.assoc sum.cong)
+    then show "poly (of_qr \<phi>) x - poly (of_qr \<phi>) u = (x - u) * ?q x"
+      by (metis (no_types, lifting) calculation mult.commute p_\<phi> poly_as_sum sum.cong)
+  qed
 qed
 
 end
