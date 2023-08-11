79c8e6dfc92659d4f60f898b2139a7a948e06b68
tobias_rothmann <ulrxz@student.kit.edu>
2023-08-11 16:19:40 +0200

    refined Poly Commit correct proof

--- KZG_correct.thy
+++ KZG_correct.thy
@@ -23,11 +23,8 @@ lemma lossless_Setup: "lossless_spmf (Setup t)"
   by (metis (no_types, lifting) G\<^sub>p.order_gt_0 lossless_bind_spmf lossless_return_spmf lossless_sample_uniform)
 
 theorem Poly_Commit_correct:  
-  assumes t_ge_2: "t\<ge>2"
-  and deg_\<phi>_let: "degree (of_qr \<phi>) \<le> t"
 shows "spmf (Poly_Commit_game t \<phi>) True = 1"
 proof -
-  note [simp] = Let_def split_def
   have weight_Setup: "weight_spmf (Setup t) = 1" using lossless_spmf_def lossless_Setup by fast
   have "(Poly_Commit_game t \<phi>) = 
    do{
@@ -36,7 +33,7 @@ proof -
     }"
   unfolding Poly_Commit_game_def Commit_def VerifyPoly_def by force
   also have "\<dots> = scale_spmf (weight_spmf (Setup t)) (return_spmf True)"
-    by (simp add:split_def)(rule bind_spmf_const)
+    by (simp add: split_def)(rule bind_spmf_const)
   also have "\<dots> = return_spmf True" using weight_Setup by force
   finally show ?thesis by fastforce
 qed
