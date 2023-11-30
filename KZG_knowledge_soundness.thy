b8daf54592d226492dc71d834a0a2c8e1488a14e
tobias-rothmann <tobias.rothmann@tum.de>
2023-11-30 11:42:26 +0100

    knowledge soundness game alt_def

--- KZG_knowledge_soundness.thy
+++ KZG_knowledge_soundness.thy
@@ -29,6 +29,32 @@ type_synonym ('a', 'e') extractor =
   "'a' commit \<Rightarrow> 'e' calc_vector \<Rightarrow> 
     'e' mod_ring poly"
 
+lemma pull_down_assert_spmf_with_return:
+"do {
+    z::'z \<leftarrow> Z:: 'z spmf;
+    x::'x \<leftarrow> (X:: 'z \<Rightarrow> 'x spmf) z;
+    _ :: unit \<leftarrow> assert_spmf((f::'z \<Rightarrow> 'x \<Rightarrow> bool) z x);
+    y::'y \<leftarrow> (Y:: 'z \<Rightarrow> 'x \<Rightarrow> 'y spmf) z x;
+    return_spmf ((g::'z \<Rightarrow> 'x \<Rightarrow> 'y \<Rightarrow> bool) z x y) }
+  = do {
+    z::'z \<leftarrow> Z:: 'z spmf;
+    x::'x \<leftarrow> (X:: 'z \<Rightarrow> 'x spmf) z;
+    y::'y \<leftarrow> (Y:: 'z \<Rightarrow> 'x \<Rightarrow> 'y spmf) z x;
+    _ :: unit \<leftarrow> assert_spmf((f::'z \<Rightarrow> 'x \<Rightarrow> bool) z x);
+    return_spmf ((g::'z \<Rightarrow> 'x \<Rightarrow> 'y \<Rightarrow> bool) z x y) }"
+proof -
+  have "\<forall>z x. do {
+    _ :: unit \<leftarrow> assert_spmf((f::'z \<Rightarrow> 'x \<Rightarrow> bool) z x);
+    y::'y \<leftarrow> (Y:: 'z \<Rightarrow> 'x \<Rightarrow> 'y spmf) z x;
+    return_spmf ((g::'z \<Rightarrow> 'x \<Rightarrow> 'y \<Rightarrow> bool) z x y) }
+  = do {
+    y::'y \<leftarrow> (Y:: 'z \<Rightarrow> 'x \<Rightarrow> 'y spmf) z x;
+    _ :: unit \<leftarrow> assert_spmf((f::'z \<Rightarrow> 'x \<Rightarrow> bool) z x);
+    return_spmf ((g::'z \<Rightarrow> 'x \<Rightarrow> 'y \<Rightarrow> bool) z x y) }"
+    using bind_commute_spmf by fast
+  then show ?thesis by presburger
+qed
+
 definition knowledge_soundness_game :: "('a, 'e) extractor \<Rightarrow> ('a, 'e) adversary_1 \<Rightarrow> ('a, 'e) adversary_2 
   \<Rightarrow> bool spmf"
   where "knowledge_soundness_game E \<A> \<A>' = TRY do {
@@ -40,17 +66,20 @@ definition knowledge_soundness_game :: "('a, 'e) extractor \<Rightarrow> ('a, 'e
   (i, \<phi>_i, w_i) \<leftarrow> \<A>' PK C calc_vec;
   return_spmf (VerifyEval PK C i \<phi>_i w_i \<and> \<phi>_i \<noteq> poly \<phi> i)} ELSE return_spmf False"
 
-(*definition bind_game :: "('a, 'e) adversary \<Rightarrow> bool spmf"
+(*
+definition bind_game :: "('a, 'e) adversary \<Rightarrow> bool spmf"
   where "bind_game \<A> = TRY do {
   PK \<leftarrow> key_gen;
   (C, i, \<phi>_i, w_i, \<phi>'_i, w'_i) \<leftarrow> \<A> PK;
   _ :: unit \<leftarrow> assert_spmf (\<phi>_i \<noteq> \<phi>'_i \<and> w_i \<noteq> w'_i \<and> valid_msg \<phi>_i w_i \<and> valid_msg \<phi>'_i w'_i); 
-    \<comment>\<open>maybe \<or> for w_i or no w_i at all?\<close>
-  let b = verify PK C i \<phi>_i w_i;
-  let b' = verify PK C i \<phi>'_i w'_i;
-  return_spmf (b \<and> b')} ELSE return_spmf False"*)
+  let b = VerifyEval PK C i \<phi>_i w_i;
+  let b' = VerifyEval PK C i \<phi>'_i w'_i;
+  return_spmf (b \<and> b')} ELSE return_spmf False"
+*)
 
-fun knowledge_soundness_reduction
+subsubsection \<open>reduction definition\<close>
+
+definition knowledge_soundness_reduction
   :: "('a, 'e) extractor \<Rightarrow> ('a, 'e) adversary_1 \<Rightarrow> ('a, 'e) adversary_2 \<Rightarrow> ('a, 'e) adversary"                     
 where
   "knowledge_soundness_reduction E \<A> \<A>' PK = do {
@@ -63,8 +92,128 @@ where
   let w'_i = createWitness PK (to_qr \<phi>) i;
   return_spmf (C, i, \<phi>_i, w_i, \<phi>'_i, w'_i)}"
 
-fun extractor :: "('a, 'e) extractor" where 
-  "extractor C calc_vec = Poly calc_vec"
+subsubsection \<open>Extractor definition\<close>
+fun E :: "('a, 'e) extractor" where 
+  "E C calc_vec = Poly calc_vec"
+
+subsubsection \<open>alternative definitions for easier proving + their equivalence proofs\<close>
+
+lemma knowledge_soundness_game_alt_def: 
+  "knowledge_soundness_game E \<A> \<A>' = 
+  TRY do {
+    PK \<leftarrow> key_gen;
+    (C,calc_vec) \<leftarrow> \<A> PK;
+    let \<phi> = E C calc_vec;
+    (i, \<phi>_i, w_i) \<leftarrow> \<A>' PK C calc_vec;
+     _ :: unit \<leftarrow> assert_spmf (length PK = length calc_vec 
+      \<and> C = fold (\<lambda> i acc. acc \<otimes>\<^bsub>G\<^sub>p\<^esub> PK!i ^\<^bsub>G\<^sub>p\<^esub> (calc_vec!i)) [0..<length PK] \<one>\<^bsub>G\<^sub>p\<^esub> 
+      \<and> VerifyEval PK C i \<phi>_i w_i \<and> \<phi>_i \<noteq> poly \<phi> i);
+   return_spmf True
+  } ELSE return_spmf False"
+proof -
+  note [simp] = Let_def split_def
+  have "do {
+  PK \<leftarrow> key_gen;
+  (C,calc_vec) \<leftarrow> \<A> PK;
+  _ :: unit \<leftarrow> assert_spmf (length PK = length calc_vec 
+                          \<and> C = fold (\<lambda> i acc. acc \<otimes>\<^bsub>G\<^sub>p\<^esub> PK!i ^\<^bsub>G\<^sub>p\<^esub> (calc_vec!i)) [0..<length PK] \<one>\<^bsub>G\<^sub>p\<^esub>);  
+  (i, \<phi>_i, w_i) \<leftarrow> \<A>' PK C calc_vec;
+  return_spmf (VerifyEval PK C i \<phi>_i w_i \<and> \<phi>_i \<noteq> poly (E C calc_vec) i)} 
+  = 
+  do {
+  PK \<leftarrow> key_gen;
+  (C,calc_vec) \<leftarrow> \<A> PK;
+  (i, \<phi>_i, w_i) \<leftarrow> \<A>' PK C calc_vec;
+  _ :: unit \<leftarrow> assert_spmf (length PK = length calc_vec 
+                          \<and> C = fold (\<lambda> i acc. acc \<otimes>\<^bsub>G\<^sub>p\<^esub> PK!i ^\<^bsub>G\<^sub>p\<^esub> (calc_vec!i)) [0..<length PK] \<one>\<^bsub>G\<^sub>p\<^esub>);  
+  return_spmf (VerifyEval PK C i \<phi>_i w_i \<and> \<phi>_i \<noteq> poly (E C calc_vec) i)}"
+    using pull_down_assert_spmf_with_return[of key_gen \<A>] by fastforce
+  then have "knowledge_soundness_game E \<A> \<A>' = 
+  TRY do {
+    PK \<leftarrow> key_gen;
+    (C,calc_vec) \<leftarrow> \<A> PK;
+    let \<phi> = E C calc_vec;
+    (i, \<phi>_i, w_i) \<leftarrow> \<A>' PK C calc_vec;
+     _ :: unit \<leftarrow> assert_spmf (length PK = length calc_vec 
+                            \<and> C = fold (\<lambda> i acc. acc \<otimes>\<^bsub>G\<^sub>p\<^esub> PK!i ^\<^bsub>G\<^sub>p\<^esub> (calc_vec!i)) [0..<length PK] \<one>\<^bsub>G\<^sub>p\<^esub>);  
+    return_spmf (VerifyEval PK C i \<phi>_i w_i \<and> \<phi>_i \<noteq> poly \<phi> i)
+  } ELSE return_spmf False"
+    unfolding knowledge_soundness_game_def by algebra
+  also have "\<dots> = TRY do {
+    PK \<leftarrow> key_gen;
+    TRY do {
+    (C,calc_vec) \<leftarrow> \<A> PK;
+    TRY do {
+    let \<phi> = E C calc_vec;
+    TRY do {
+    (i, \<phi>_i, w_i) \<leftarrow> \<A>' PK C calc_vec;
+    TRY do {
+     _ :: unit \<leftarrow> assert_spmf (length PK = length calc_vec 
+                            \<and> C = fold (\<lambda> i acc. acc \<otimes>\<^bsub>G\<^sub>p\<^esub> PK!i ^\<^bsub>G\<^sub>p\<^esub> (calc_vec!i)) [0..<length PK] \<one>\<^bsub>G\<^sub>p\<^esub>);  
+    TRY return_spmf (VerifyEval PK C i \<phi>_i w_i \<and> \<phi>_i \<noteq> poly \<phi> i)  ELSE return_spmf False
+    } ELSE return_spmf False
+    } ELSE return_spmf False
+    } ELSE return_spmf False
+    } ELSE return_spmf False
+  } ELSE return_spmf False"
+   unfolding split_def Let_def
+   by (fold try_bind_spmf_lossless2[OF lossless_return_spmf])simp
+  also have "\<dots> = TRY do {
+    PK \<leftarrow> key_gen;
+    TRY do {
+    (C,calc_vec) \<leftarrow> \<A> PK;
+    TRY do {
+    let \<phi> = E C calc_vec;
+    TRY do {
+    (i, \<phi>_i, w_i) \<leftarrow> \<A>' PK C calc_vec;
+    TRY do {
+     _ :: unit \<leftarrow> assert_spmf (length PK = length calc_vec 
+                            \<and> C = fold (\<lambda> i acc. acc \<otimes>\<^bsub>G\<^sub>p\<^esub> PK!i ^\<^bsub>G\<^sub>p\<^esub> (calc_vec!i)) [0..<length PK] \<one>\<^bsub>G\<^sub>p\<^esub>);  
+  _ :: unit \<leftarrow> assert_spmf (VerifyEval PK C i \<phi>_i w_i \<and> \<phi>_i \<noteq> poly \<phi> i);
+   return_spmf True
+    } ELSE return_spmf False
+    } ELSE return_spmf False
+    } ELSE return_spmf False
+    } ELSE return_spmf False
+  } ELSE return_spmf False"
+   by(auto simp add: try_bind_assert_spmf try_spmf_return_spmf1 intro!: try_spmf_cong bind_spmf_cong)
+  also have "\<dots> = TRY do {
+    PK \<leftarrow> key_gen;
+    TRY do {
+    (C,calc_vec) \<leftarrow> \<A> PK;
+    TRY do {
+    let \<phi> = E C calc_vec;
+    TRY do {
+    (i, \<phi>_i, w_i) \<leftarrow> \<A>' PK C calc_vec;
+    TRY do {
+     _ :: unit \<leftarrow> assert_spmf (length PK = length calc_vec 
+                            \<and> C = fold (\<lambda> i acc. acc \<otimes>\<^bsub>G\<^sub>p\<^esub> PK!i ^\<^bsub>G\<^sub>p\<^esub> (calc_vec!i)) [0..<length PK] \<one>\<^bsub>G\<^sub>p\<^esub> 
+      \<and> VerifyEval PK C i \<phi>_i w_i \<and> \<phi>_i \<noteq> poly \<phi> i);
+   return_spmf True
+    } ELSE return_spmf False
+    } ELSE return_spmf False
+    } ELSE return_spmf False
+    } ELSE return_spmf False
+  } ELSE return_spmf False" 
+    using assert_anding by presburger
+  also have "\<dots> = TRY do {
+    PK \<leftarrow> key_gen;
+    (C,calc_vec) \<leftarrow> \<A> PK;
+    let \<phi> = E C calc_vec;
+    (i, \<phi>_i, w_i) \<leftarrow> \<A>' PK C calc_vec;
+     _ :: unit \<leftarrow> assert_spmf (length PK = length calc_vec 
+      \<and> C = fold (\<lambda> i acc. acc \<otimes>\<^bsub>G\<^sub>p\<^esub> PK!i ^\<^bsub>G\<^sub>p\<^esub> (calc_vec!i)) [0..<length PK] \<one>\<^bsub>G\<^sub>p\<^esub> 
+      \<and> VerifyEval PK C i \<phi>_i w_i \<and> \<phi>_i \<noteq> poly \<phi> i);
+   return_spmf True
+  } ELSE return_spmf False" 
+   unfolding split_def Let_def
+    by(fold try_bind_spmf_lossless2[OF lossless_return_spmf]) simp
+  finally show ?thesis .
+qed
+
+
+subsection \<open>Reduction proof\<close>
+
 
 theorem "knowledge_soundness_game E \<A> \<A>' = bind_game (knowledge_soundness_reduction E \<A> \<A>')"
   sorry
