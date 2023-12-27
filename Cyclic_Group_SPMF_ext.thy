bc4deb38bf7d2c7386c26be357b9bc2fc9a46f36
tobias-rothmann <tobias.rothmann@tum.de>
2023-12-27 10:51:41 +0100

    Cylic group extensiion done

--- Cyclic_Group_SPMF_ext.thy
+++ Cyclic_Group_SPMF_ext.thy
@@ -242,7 +242,27 @@ proof -
    qed
    finally show ?thesis .
  qed
-  
 
+lemma weight_sample_distinct_coordinates_uniform_list: "weight_spmf (sample_distinct_coordinates_uniform k n) = of_bool (k\<le>n \<and> (k=0 \<or> 0<n))"
+  by (auto simp add: sample_distinct_coordinates_uniform_def weight_spmf_of_set lossless_weight_spmfD weight_sample_distinct_uniform_list split: split_indicator)
+
+lemma weight_sample_distinct_coordinates_uniform_list_k_0 [simp]: "weight_spmf (sample_distinct_coordinates_uniform 0 n) = 1"
+  by (auto simp add: weight_sample_distinct_coordinates_uniform_list)
+
+lemma weight_sample_distinct_coordinates_uniform_list_n_0 [simp]: "weight_spmf (sample_distinct_coordinates_uniform k 0) = of_bool (k=0)"
+  by (auto simp add: weight_sample_distinct_coordinates_uniform_list)
+
+lemma weight_sample_distinct_coordinates_uniform_list_k_le_n [simp]: "k<n \<Longrightarrow> weight_spmf (sample_distinct_coordinates_uniform k n) = 1"
+  by (auto simp add: weight_sample_distinct_coordinates_uniform_list less_iff_Suc_add)
+
+lemma lossless_sample_sample_distinct_coordinates_uniform_list [simp]: "lossless_spmf (sample_distinct_coordinates_uniform k n) \<longleftrightarrow> k\<le>n"
+  by (auto simp add: lossless_spmf_def weight_sample_distinct_coordinates_uniform_list intro: ccontr)
+
+lemma set_spmf_sample_distinct_coordinates_uniform_list [simp]: 
+  "set_spmf (sample_distinct_coordinates_uniform k n) 
+  = pair_lists `
+    ({xs. length xs = k \<and> distinct xs \<and> set xs \<subseteq> {..<n}} \<times>
+     {x. set x \<subseteq> {..<n} \<and> length x = k})"
+  by (simp add: sample_distinct_coordinates_uniform_def)
 
 end
